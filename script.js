0f9bae847b20ca7f9f139d3296228050e0f2b2b5
scrol <lopikop008@gmail.com>
2023-08-21 13:19:32 +0500

    first

---
+++ script.js
@@ -0,0 +1,52 @@
+/* console.log(1); */
+
+/* console.log('String');
+console.log(212.5);
+console.log(true);
+console.log(false); */
+ 
+
+/* var names = 'shaha'
+var years = 35
+var boolean = true
+var und 
+var allInfo = ' Я ' + names + ' мне ' + years + ' лет!' */
+
+/* console.log(names);
+console.log(years);
+console.log(boolean);
+console.log(und); */
+
+/* console.log(allInfo); */
+
+/* console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(16 / 3);
+console.log(16 % 3); */
+
+
+/* console.log(' 6 + 3 = ' + (6 + 3));
+console.log(' 6 - 3 = ' + (6 - 3));
+console.log(' 76 * 3 = ' + (6 * 3));
+console.log(' 86 % 3 = ' + (6 % 3)); */
+
+/* var god = +prompt('Какой сейчас год')
+var str = +prompt('Какого вы года рождения')
+
+console.log( god - str); */
+
+var names = prompt('Введите ваш псевдоним')
+var old = prompt('Введите ваш возраст')
+var primerOne = +prompt('Решите пример: 199+1')
+var primerTwo = +prompt('Решите пример: 199-99')
+var primerThree = +prompt('Решите пример: 180/3')
+var primerFour = +prompt('Решите пример: 199 % 99')
+var congrat =  prompt('Загляните в консоль с помощью Ctrl+Shift+j')
+
+console.log( 'Меня зовут ' + names);
+console.log( 'Мне ' + old + ' лет');
+console.log('Пример номер 1 Ответ = 200, Ваш ответ = ' + primerOne);
+console.log('Пример номер 2 Ответ = 100, Ваш ответ = ' + primerTwo);
+console.log('Пример номер 3 Ответ = 60, Ваш ответ = ' + primerThree);
+console.log('Пример номер 1 Ответ = 2.01010101, Ваш ответ = ' + primerFour);
